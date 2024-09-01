6ecafc443af4ece581f3abadb07b42cdbaaad045
Eduardo <edufcarvalho@gmail.com>
2024-09-01 15:11:58 -0300

    feat: create a model with migration

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    // protected $table = 'categorias'; // Para customizar o nome da tabela caso já exista e seja em portugues por exemplo
+}
